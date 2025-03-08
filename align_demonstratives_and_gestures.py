f38b8747d7741b36fc570cb536715cd415325372
pgeorgis <philip.georgis@gmail.com>
2025-03-09 00:57:38 +0100

    Find nearest gesture apices to selected words' (demonstrative pronouns) onsets

--- align_demonstratives_and_gestures.py
+++ align_demonstratives_and_gestures.py
@@ -1,13 +1,33 @@
 import json
 import os
 from statistics import mean
+from typing import Callable
 
-from constants import ASR_MODEL_PATH, AUDIO_PATH, TRANCRIPT_PATH, VIDEO_PATH
+from constants import (ASR_MODEL_PATH, AUDIO_PATH, DEMONSTRATIVE_PRONOUNS,
+                       TRANCRIPT_PATH, VIDEO_PATH)
 from extract_gesture import (detect_gesture_apices,
                              detect_hand_gestures_in_video,
                              get_hands_detection_model)
 from extract_speech import speech_to_text
 from process_video import extract_frames_by_timestamp
+
+def get_word_onsets_from_asr_results(asr_results: dict, filter_func: Callable) -> list:
+    """Get word onset timings for selected words in ASR results."""
+    filtered_word_timings = []
+    for entry in asr_results["result"]:
+        if filter_func(entry.get("word", "")):
+            filtered_word_timings.append((entry.get("word"), entry.get("start")))
+    return filtered_word_timings
+
+
+def find_nearest_gesture_to_words(word_onsets: list, gesture_apices: dict) -> dict:
+    """Find nearest gesture (by apex timestamp) to each word's onset."""
+    nearest_gestures = {}
+    for word, onset in word_onsets:
+        nearest_gesture = min(gesture_apices.keys(), key=lambda x: abs(onset - gesture_apices[x]))
+        gesture_apex = gesture_apices[nearest_gesture]
+        nearest_gestures[(word, onset)] = (nearest_gesture, gesture_apex)
+    return nearest_gestures
 
 # Perform speech-to-text conversion
 asr_results = speech_to_text(AUDIO_PATH, model_path=ASR_MODEL_PATH, chunk_duration_seconds=0.1)
@@ -24,11 +44,30 @@ gestures = detect_hand_gestures_in_video(VIDEO_PATH, hands_detection_model)
 gesture_apices = detect_gesture_apices(gestures)
 
 # Get averaged timestamp of each gesture's apex candidates
-apex_timestamps = [mean(gesture_apex.values()) for gesture_apex in gesture_apices.values()]
+#apex_timestamps = {idx: mean(gesture_apex.values()) for idx, gesture_apex in gesture_apices.items()}
+# Get timestamp of each gesture's apex candidates
+apex_timestamps = {
+    (idx, apex_type): apex_timestamp
+    for idx, gesture in gesture_apices.items()
+    for (apex_type, apex_timestamp) in gesture.items()
+}
 
-# Save video frames from apex timestamps
+# Get timestamps of demonstrative pronoun onsets from ASR results
+demonstrative_onsets = get_word_onsets_from_asr_results(
+    asr_results,
+    filter_func=lambda x: any(demonstr_regex.match(x) for demonstr_regex in DEMONSTRATIVE_PRONOUNS)
+)
+
+# Find nearest gesture to each pronoun's onset
+nearest_gestures_to_demonstratives = find_nearest_gesture_to_words(demonstrative_onsets, apex_timestamps)
+
+# Save video frames from nearest gesture apex timestamps
+nearest_gesture_timestamps = [
+    apex_timestamp
+    for _, apex_timestamp in nearest_gestures_to_demonstratives.values()
+]
 extract_frames_by_timestamp(
     video_path=VIDEO_PATH,
-    timestamps=apex_timestamps,
+    timestamps=nearest_gesture_timestamps,
     output_folder=os.path.join("data", "video_frames")
 )
--- constants.py
+++ constants.py
@@ -5,9 +5,9 @@ import re
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s: %(message)s')
 
 DEMONSTRATIVE_PRONOUNS = {
-    r"das",
-    r"da(hinte[nr]|rüber)?",
-    r"dies(e([mnrs])?)?",
+    r"das$",
+    #r"da(hinte[nr]|rüber)$",
+    r"dies(e([mnrs])?)?$",
 }
 DEMONSTRATIVE_PRONOUNS = {re.compile(pattern, re.IGNORECASE) for pattern in DEMONSTRATIVE_PRONOUNS}
 

325fc1ef482f8d8109b2b6ea594aabb9b4559b5f
pgeorgis <philip.georgis@gmail.com>
2025-03-08 20:55:14 +0100

    Add constants with default data paths

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import re
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s: %(message)s')
@@ -9,3 +10,8 @@ DEMONSTRATIVE_PRONOUNS = {
     r"dies(e([mnrs])?)?",
 }
 DEMONSTRATIVE_PRONOUNS = {re.compile(pattern, re.IGNORECASE) for pattern in DEMONSTRATIVE_PRONOUNS}
+
+ASR_MODEL_PATH = os.path.join("models", "de", "vosk-model-de-0.21")  # TODO model needs to be added to git or downloaded
+AUDIO_PATH = os.path.join("data", "audio.wav")
+TRANCRIPT_PATH = os.path.join("data", "transcription.json")
+VIDEO_PATH = os.path.join("data", "video.mp4")
